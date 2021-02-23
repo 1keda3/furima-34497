c19fdb7ef796b2313a40fe1d61669eed512c4260
1keda3 <xrzrutosa@yahoo.co.jp>
2021-02-23 12:15:56 +0900

    新規登録

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   with_options presence: true do
-    validates :nickname, format: {message: "is invalid. Input nickname characters."}
+    validates :nickname
     validates :last_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: "is invalid. Input full-width characters."}
     validates :first_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: "is invalid. Input full-width characters."}
     validates :last_name_kana, format: {with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
     validates :first_name_kana, format: {with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
-    validates :birthday, format: {message: "is invalid. Input birthday characters."}
+    validates :birthday
   end
+  
 end
