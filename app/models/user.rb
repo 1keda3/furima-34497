c6784446ec3b1fb7699b032bd1f2e0e4997b5bac
1keda3 <xrzrutosa@yahoo.co.jp>
2021-02-26 10:55:14 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  japanese = /\A[ぁ-んァ-ヶ一-龥々]+\z/
+  furigana = /\A[ァ-ヶー－]+\z/
+
   with_options presence: true do
     validates :nickname
-    validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'is invalid. Input full-width characters.' }
-    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'is invalid. Input full-width characters.' }
-    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters.' }
-    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters.' }
+    validates :last_name, format: { with: japanese, message: 'is invalid. Input full-width characters.' }
+    validates :first_name, format: { with: japanese, message: 'is invalid. Input full-width characters.' }
+    validates :last_name_kana, format: { with: furigana, message: 'is invalid. Input full-width katakana characters.' }
+    validates :first_name_kana, format: { with: furigana, message: 'is invalid. Input full-width katakana characters.' }
     validates :birthday
     validates :password, format: { with: /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]+\z/, message: 'Include both letters and numbers.' }
   end
