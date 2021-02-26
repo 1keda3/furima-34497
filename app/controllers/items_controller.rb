1c89bbf7aa6f088be2c7d999ebeb1405cddcf012
1keda3 <xrzrutosa@yahoo.co.jp>
2021-02-26 14:11:01 +0900

    ログイン状態のユーザーは商品出品ページへ遷移できる

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,21 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
+
   def index
+    @items = Item.all
+  end
+
+  def new
+   @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :image, :text)
   end
 end
