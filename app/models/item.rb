0031308eaa0e592c5e77d824f70509f4aefec6d3
1keda3 <xrzrutosa@yahoo.co.jp>
2021-03-02 14:50:43 +0900

    specファイルの微修正とRubocop

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,15 +2,15 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :image
     validates :title
-    validates :price, numericality: { only_integer: true, greater_than: 299, less_than: 10000000 }
+    validates :price, numericality: { only_integer: true, greater_than: 299, less_than: 10_000_000 }
     validates :catch_copy
 
     with_options numericality: { other_than: 1 } do
-     validates :category_id
-     validates :state_id
-     validates :postage_id
-     validates :address_id
-     validates :delivery_id
+      validates :category_id
+      validates :state_id
+      validates :postage_id
+      validates :address_id
+      validates :delivery_id
     end
   end
 
