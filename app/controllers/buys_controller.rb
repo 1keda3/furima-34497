e3c542d0467b32edd8caac52238e6d8834b6d8ce
1keda3 <xrzrutosa@yahoo.co.jp>
2021-03-05 11:28:20 +0900

    住所情報

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -4,10 +4,25 @@ class BuysController < ApplicationController
     @item = Item.find(params[:item_id])
   end
 
+  def new
+    @buy = BuyForm.new
+  end
+
+  def create
+    @buy = BuyForm.new(set_params)
+    if @buy.valid?
+      @buy.save
+    end
+  end
+
   private
 
   def buy_item_params
     params.require(:item).permit(:title, :price, :postage_id)
+  end
+
+  def set_params
+    params.permit(:telephone, :postcode, :prefecture_id, :city, :block, :building, :buy, :user, :item)
   end
 
 end
